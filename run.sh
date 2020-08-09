a3d1a0f1aa6393415fe7b2e96acda378e39c1d87
pat <pat@p15r.net>
2020-08-09 11:03:25 +0200

    cleanup

--- run.sh
+++ run.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 
-mkdir output
+# set
+# -e            exit on error
+# -u            treat unset variables as an error
+# -f            disable filename expansion (globbing)
+# -o pipefail   the return value of a pipeline is the value of the last (rightmost)
+#                   command to exit with a non-zero status
 
-docker run --name hyok-wrapper \
+set -euf -o pipefail
+
+output_dir="output"
+if [ ! -d "$output_dir" ]; then
+    mkdir $output_dir
+fi
+
+docker create --name hyok-wrapper \
     -p 0.0.0.0:443:443/tcp \
     -v "$(pwd)"/output:/opt/hyok-wrapper/output \
     pat/hyok-wrapper:0.1
+
+docker start hyok-wrapper

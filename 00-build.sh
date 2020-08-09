5123cf7bab29968e3ec23a31bb0f74631b8455bb
pat <pat@p15r.net>
2020-08-09 17:14:27 +0200

    create dir "docker/certs" before generating certs

--- 00-build.sh
+++ 00-build.sh
@@ -9,6 +9,13 @@
 
 set -euf -o pipefail
 
+cert_dir="docker/certs"
+if [ ! -d "$cert_dir" ]; then
+    mkdir $cert_dir
+    # make it accessible for gunicorn
+    chmod o+rx $cert_dir
+fi
+
 openssl req -x509 -nodes -days 999 -newkey rsa:2048 -keyout docker/certs/nginx.key -out docker/certs/nginx.crt -subj "/C=SC/ST=SomeRegion/L=Some Valey/O=SomeOrg/OU=SomeOrgUnit/CN=somecommonname"
 chmod o+r docker/certs/nginx.key docker/certs/nginx.crt
 

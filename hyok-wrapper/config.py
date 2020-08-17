09add6ff9fa50feb19d9da8e6b7454efb00d9a4a
pat <pat@p15r.net>
2020-08-17 22:48:53 +0200

    move configurable variables into a json-based config

--- hyok-wrapper/config.py
+++ hyok-wrapper/config.py
@@ -3,13 +3,14 @@
 import json
 
 
-def get_config() -> dict:
+def get_config(key: str) -> str:
     try:
         with open('/opt/hyok-wrapper/config/config.json', 'r') as f:
             cfg = json.load(f)
     except FileNotFoundError as e:
         print('Cannot load config file. Has "02-load-config.sh" already been executed?')
         print(e)
-        return {}
+        return ''
 
-    return cfg
+    # Let this raise KeyError if key does not exist
+    return cfg[key]
--- hyok-wrapper/vault_backend.py
+++ hyok-wrapper/vault_backend.py
@@ -2,14 +2,17 @@
 
 import base64
 import hvac
+import config
 
 
-def get_dynamic_secret() -> str:
-    client = hvac.Client(url='http://vault:8200', token='root')
+def get_dynamic_secret() -> bytes:
+    vault_url = config.get_config('VAULT_URL')
+    vault_token = config.get_config('VAULT_TOKEN')
+    client = hvac.Client(url=vault_url, token=vault_token)
 
     if not client.sys.is_initialized():
         print('Vault has not been initialized.')
-        return ''
+        return b''
 
     key_name = 'salesforce'
 
@@ -19,14 +22,14 @@ def get_dynamic_secret() -> str:
     except Exception as e:
         print(f'Transit key "{key_name}" cannot be read.')
         print(e)
-        return ''
+        return b''
 
     try:
         latest_version = response['data']['latest_version']
     except KeyError as e:
         print(f'Cannot get latest version of key "{key_name}":')
         print(e)
-        return ''
+        return b''
 
     # fetch key
     response = client.secrets.transit.export_key(
@@ -37,6 +40,6 @@ def get_dynamic_secret() -> str:
     except KeyError as e:
         print(f'Cannot get key "{key_name}":')
         print(e)
-        return ''
+        return b''
 
-    return base64.b64decode(b64_key).decode()
+    return base64.b64decode(b64_key)

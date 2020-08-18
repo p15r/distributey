5c7bec6ac147e37d7962a94cc59907834f09b304
pat <pat@p15r.net>
2020-08-18 22:23:42 +0200

    Implement basic logging (https://github.com/p15r/HYOK-Wrapper/issues/6)

--- hyok-wrapper/config.py
+++ hyok-wrapper/config.py
@@ -1,15 +1,18 @@
 # Loads json config
 
 import json
+import logging
 
 
 def get_config_by_key(key: str) -> str:
+    logger = logging.getLogger(__name__)
+
     try:
         with open('/opt/hyok-wrapper/config/config.json', 'r') as f:
             cfg = json.load(f)
     except FileNotFoundError as e:
-        print('Cannot load config file. Has "02-load-config.sh" already been executed?')
-        print(e)
+        logger.error('Cannot load config file. Has "02-load-config.sh" already been executed?')
+        logger.error(e)
         return ''
 
     # Let this raise KeyError if key does not exist
--- hyok-wrapper/vault_backend.py
+++ hyok-wrapper/vault_backend.py
@@ -3,30 +3,33 @@
 import base64
 import hvac
 import config
+import logging
 
 
 def get_dynamic_secret(key: str) -> bytes:
+    logger = logging.getLogger(__name__)
+
     vault_url = config.get_config_by_key('VAULT_URL')
     vault_token = config.get_config_by_key('VAULT_TOKEN')
     client = hvac.Client(url=vault_url, token=vault_token)
 
     if not client.sys.is_initialized():
-        print('Vault has not been initialized.')
+        logger.error('Vault has not been initialized.')
         return b''
 
     # fetch latest version of key
     try:
         response = client.secrets.transit.read_key(name=key)
     except Exception as e:
-        print(f'Transit key "{key}" cannot be read.')
-        print(e)
+        logger.error(f'Transit key "{key}" cannot be read.')
+        logger.error(e)
         return b''
 
     try:
         latest_version = response['data']['latest_version']
     except KeyError as e:
-        print(f'Cannot get latest version of key "{key}":')
-        print(e)
+        logger.error(f'Cannot get latest version of key "{key}":')
+        logger.error(e)
         return b''
 
     # fetch key
@@ -36,8 +39,8 @@ def get_dynamic_secret(key: str) -> bytes:
     try:
         b64_key = response['data']['keys'][str(latest_version)]
     except KeyError as e:
-        print(f'Cannot get key "{key}":')
-        print(e)
+        logger.error(f'Cannot get key "{key}":')
+        logger.error(e)
         return b''
 
     return base64.b64decode(b64_key)

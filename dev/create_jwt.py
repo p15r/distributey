98b1ec8605c16f8e07573566aa6f71dfe4096e0b
Andreas Gruhler <andreas.gruhler@adfinis.com>
2020-09-01 17:13:54 +0200

    use Vault for jwt auth

--- dev/create_jwt.py
+++ dev/create_jwt.py
@@ -7,10 +7,13 @@ private_key = open('mtls/myCert-nopwd.key').read()
 
 payload = {
     'sub': 'salesforce-cacheonlyservice',
+    'subj': 'salesforce-cacheonlyservice',
+    'iss': 'salesforce',
     'aud': 'urn:hyok-wrapper',
+    'iat': datetime.datetime.utcnow(),
     'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=expiration_time)
 }
 
 token = jwt.encode(payload, private_key, algorithm='RS256', headers=dict(kid='my-key-id')).decode('utf-8')
 
-print(f'Token: Bearer {token}\nThis token expires in {expiration_time/60} minutes.')
+print(f'Bearer {token}')
---
+++ docker/terraform/policies/salesforce-cacheonlyservice-transit.hcl
@@ -0,0 +1,7 @@
+path "transit/export/encryption-key/salesforce*" {
+   capabilities =  ["read"]
+}
+
+path "transit/keys/salesforce" {
+   capabilities =  ["read"]
+}

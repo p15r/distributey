422bc941a16abe2b667904fb6c2f17eb2de50435
pat <pat@p15r.net>
2020-09-16 17:38:56 +0200

    Remove old config before copying new config

--- 02-load-config.sh
+++ 02-load-config.sh
@@ -9,10 +9,26 @@
 
 set -euf -o pipefail
 
+
+# Create backup of existing config & copy new config to container
+timestamp=$(date +%Y-%m-%d_%H:%M:%S)
+
+echo "🔧 Create \"config/\" directory if it does not exist.."
+docker exec hyok-wrapper sh -c "[ ! -d /opt/hyok-wrapper/config/ ] && mkdir -p /opt/hyok-wrapper/config/init || echo \"(Config directory already exists)\""
+
+echo "💾 Create backup of current config.."
+docker exec hyok-wrapper sh -c "mkdir /opt/hyok-wrapper/config-$timestamp && cp -r /opt/hyok-wrapper/config/* /opt/hyok-wrapper/config-$timestamp"
+
+echo "🗑️ Delete current config.."
+docker exec -u root hyok-wrapper sh -c "rm -rf /opt/hyok-wrapper/config/*"
+
+echo "💾 Copy new config.."
+docker cp config/ hyok-wrapper:/opt/hyok-wrapper/
+
+echo "🔧 Set file permissions.."
+docker exec -u root hyok-wrapper chown -R hyok:hyok /opt/hyok-wrapper/config
+
 # gunicorn only reloads if a python file changes..
 touch hyok-wrapper/app.py
 docker cp hyok-wrapper/app.py hyok-wrapper:/opt/hyok-wrapper/app.py
 docker exec -u root hyok-wrapper chown -R hyok:hyok /opt/hyok-wrapper/app.py
-
-docker cp config/ hyok-wrapper:/opt/hyok-wrapper
-docker exec -u root hyok-wrapper chown -R hyok:hyok /opt/hyok-wrapper/config

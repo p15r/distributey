69f426ec267d5320e948598be9d42f6d3be9dc9d
pat <pat@p15r.net>
2020-10-01 11:40:05 +0200

    cleanup

--- hyok-wrapper/app.py
+++ hyok-wrapper/app.py
@@ -11,21 +11,18 @@ import jwe
 import config
 from hyok_logging import logger
 
+
 app = Flask(__name__)
 
 logger.info(f'🚀 HYOK Wrapper is starting (pid {getpid()})...')
 
 # URL-based API versioning
-root_path = '/'
+base_path = '/'
 api_versioning_path = 'v1/'
-path_prefix = root_path + api_versioning_path
+path_prefix = base_path + api_versioning_path
 
 
 def _get_kid_from_jwt(token: str) -> str:
-    # base64 decode token
-    # token = json.loads(token)
-    # return token['kid']
-
     try:
         protected_header_unverified = jwt.get_unverified_header(token)
     except jwt.DecodeError as e:
@@ -40,13 +37,13 @@ def _get_jwt_from_header(header: EnvironHeaders) -> str:
     try:
         token = header['Authorization']
     except KeyError:
-        logger.error('Request is missing "Authorization" header. Cannot authorize request.')
+        logger.error('Request is missing "Authorization" header.')
         logger.debug(f'Malformed header: {header}')
         return ''
 
     if not token.startswith('Bearer'):
         logger.error('Cannot fetch Bearer token from Authorization header.')
-        logger.debug(f'Authorization header w/o Bearer: {token}')
+        logger.debug(f'Malformed token w/o Bearer: {token}')
         return ''
 
     token = token.split('Bearer')[1].strip()
@@ -58,14 +55,15 @@ def _authenticate(tenant: str, header: EnvironHeaders) -> str:
     """
     Authentication requires a bearer token in JWT format.
 
-    Toke must..
+    The token must..
     - have a valid signature,
-    - contain 'iss': 'salesforce' & 'sub': 'cacheonlyservice',
-    - not have been expired yet
+    - contain 'iss' & 'sub' claims
+    - not have expired (leeway of 10s for clock skew is tolerated)
 
-    JWT signature verification might not be required, because this is done by Vault as well.
-    However, it might be useful to implement a vault token<->jwt cache,
-    which requires to authenticate in HYOK Wrapper too.
+    JWT signature verification might not be required at this point,
+    because it is done by Vault as well.
+    However, it might be useful to implement a vault access token<->jwt cache,
+    which requires to authenticate here as well.
     """
 
     if not (token := _get_jwt_from_header(header)):
@@ -80,27 +78,23 @@ def _authenticate(tenant: str, header: EnvironHeaders) -> str:
 
     logger.info(f'Attempting to authenticate JWT with kid "{jwt_kid}"...')
 
-    try:
-        validation_cert = config.get_jwt_validation_cert_by_tenant_and_kid(tenant, jwt_kid)
-    except KeyError as e:
-        logger.error(f'Cannot find tenant or jwt kid in config: {e}.')
-        return ''
-
-    if not validation_cert:
+    if not (validation_cert := config.get_jwt_validation_cert_by_tenant_and_kid(tenant, jwt_kid)):
         logger.error(
-            f'No validation certificate exists in config.json '
+            'No validation certificate exists in config.json '
             f'to verify signature for JWTs with kid "{jwt_kid}".')
         return ''
+
+    logger.debug(f'Attempting to validate JWT signature using cert "{validation_cert}".')
 
     # pyjwt[crypto] requires cert to be in PEM format & only the public key.
     # TODO: Extract key from cert programmatically:
     #       https://pyjwt.readthedocs.io/en/latest/faq.html
     #           how-can-i-extract-a-public-private-key-from-a-x509-certificate
-    cert = open(validation_cert).read()
-
-    if not cert:
+    try:
+        cert = open(validation_cert).read()
+    except Exception as e:
         logger.error(
-            f'Cannot read public key at "{cert}". Make sure its format is PEM.')
+            f'Cannot read public key at "{validation_cert}" for JWT signature validation: {e}.')
         return ''
 
     logger.debug(f'Received JWT: {token}')
@@ -143,11 +137,11 @@ def _authenticate(tenant: str, header: EnvironHeaders) -> str:
 
 
 @app.route(path_prefix + '<string:tenant>/<string:jwe_kid>', methods=['GET'])
-def get_jwe_token(tenant: str = '', jwe_kid: str = ''):
+def get_wrapped_key(tenant: str = '', jwe_kid: str = ''):
     """
     tenant: Tenant (key consumer) that makes a request. E.g. Salesforce. Mandatory.
     jwe_kid: kid provided by Salesforce. Mandatory.
-    nonce: Nonce (?requestId=x) provided by Salesforce (prevent replay attacks). Optional.
+    nonce: Nonce (?requestId=x) provided by Salesforce (to prevent replay attacks). Optional.
     """
     token = _authenticate(tenant, request.headers)
 
@@ -157,18 +151,14 @@ def get_jwe_token(tenant: str = '', jwe_kid: str = ''):
         except KeyError:
             jwt_audience = 'unknown'
 
-        # https://tools.ietf.org/html/rfc6750#section-3
+        # WWW-Authenticate header according to: https://tools.ietf.org/html/rfc6750#section-3
         return Response(
             response=f'Unauthorized request from {request.headers["X-Real-Ip"]}, ({request.user_agent}).',
             status=401,
             content_type='text/html; charset=utf-8',
             headers={'WWW-Authenticate': f'Bearer scope="{jwt_audience}"'})
 
-    request_args = []
-    for key in request.args:
-        request_args.append(f'{key}: {request.args.get(key)}')
-
-    logger.info(f'Processing request (path: "{request.path}", args: "{request_args}"...')
+    logger.info(f'Processing request (path: "{request.path}", args: "{request.args.to_dict()}"...')
 
     json_jwe_token = jwe.get_wrapped_key_as_jwe(
         token,
@@ -180,9 +170,9 @@ def get_jwe_token(tenant: str = '', jwe_kid: str = ''):
         return 'Oops, internal error.', 500
 
     logger.info(
-        f'Response JWE token with kid "{json.loads(json_jwe_token)["kid"]}" sent.',)
+        f'JWE token with kid "{json.loads(json_jwe_token)["kid"]}" sent.',)
     logger.debug(
-        f'Reponse JWE token sent: {json_jwe_token}',)
+        f'JWE token: {json_jwe_token}',)
 
     resp = Response(
         response=json_jwe_token,

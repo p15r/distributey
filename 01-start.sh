c503576ddcc1cdfc83ae809e7afd0b54705d4062
pat <pat@p15r.net>
2020-10-01 14:36:08 +0200

    cleanup

--- 01-start.sh
+++ 01-start.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
-# set
-# -e            exit on error
-# -u            treat unset variables as an error
-# -f            disable filename expansion (globbing)
-# -o pipefail   the return value of a pipeline is the value of the last (rightmost)
-#                   command to exit with a non-zero status
-
 set -euf -o pipefail
 
-# Workaround: make nginx config & TLS files accessible in container
+# Make nginx config & TLS files accessible in container
 chmod o+x docker/certs/
 chmod -R o+r docker/nginx.conf docker/certs/
 
@@ -42,14 +35,6 @@ echo '🛫 Starting containers...'
 cd docker
 docker-compose up -d
 cd ..
-
-# Manually provision Vault like:
-# sleep 2 # give Vault time to start
-# Enable dynamic secrets:
-# docker exec vault vault secrets enable transit
-# Create Salesforce secret & mark it exportable:
-# docker exec vault vault write transit/keys/salesforce exportable=true
-# Verify: `docker exec vault vault read transit/keys/salesforce`
 
 echo 'ℹ️  Container processes:'
 docker ps -a

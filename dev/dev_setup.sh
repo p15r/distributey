bfe957e8f822fe9b48ec4d6755e4008a7dd617b5
pat <pat@p15r.net>
2020-10-22 22:08:10 +0200

    move dev setup tasks from 00-build.sh into own script

---
+++ dev/dev_setup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+function help {
+    echo -e "-h\tShow help."
+    echo -e "-d\tEnable developer mode."
+    exit 0
+}
+
+dev_mode=false
+
+while getopts hd flag
+do
+    case "${flag}" in
+        h) help;;
+        d) dev_mode=true;;
+    esac
+done
+
+
+echo "⛏️  Building container images..."
+docker-compose build
+
+if [ "$dev_mode" = true ] ; then
+    ./dev/dev_setup.sh
+fi

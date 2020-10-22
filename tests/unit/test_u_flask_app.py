990cba22490dd26e03d77c69c82086ab178dcc8d
pat <pat@p15r.net>
2020-10-22 21:46:02 +0200

    cleanup

--- tests/unit/test_u_flask_app.py
+++ tests/unit/test_u_flask_app.py
@@ -1,5 +1,4 @@
 import app
-import config
 
 
 class TestUnitFlaskApp():
@@ -10,21 +9,14 @@ class TestUnitFlaskApp():
         assert app._get_jwt_from_header(get_headers) == get_jwt
 
     def test__decode_jwt(self, monkeypatch, get_jwt, get_jwt_signing_pubkey):
-        monkeypatch.setattr(config, 'CFG_PATH', 'config/config.json')
-
         assert app._decode_jwt('salesforce', get_jwt, get_jwt_signing_pubkey) == \
             ('cacheonlyservice', 'salesforce')
 
     def test__authenticate(self, monkeypatch, get_headers, get_jwt):
-        monkeypatch.setattr(config, 'CFG_PATH', 'config/config.json')
-
         assert app._authenticate('salesforce', get_headers) == get_jwt
 
     def test_get_wrapped_key_no_auth(self, http_client, monkeypatch):
-        # integration test runs here, because it has no runtime deps
-
-        monkeypatch.setattr(config, 'CFG_PATH', 'config/config.json')
-
+        # this integration test runs here, because it has no runtime deps
         test_url = '/v1/salesforce/jwe-kid-salesforce-serviceX?resourceId=randomstring'
         test_headers = {'X_REAL_IP': '127.0.0.1'}
 

7b8f052c3ccdd7f7773213b18b2abea801548ccb
pat <pat@p15r.net>
2020-10-23 17:35:15 +0200

    add more coverage for _get_kid_from_jwt()

--- tests/unit/test_u_flask_app.py
+++ tests/unit/test_u_flask_app.py
@@ -5,6 +5,18 @@ class TestUnitFlaskApp():
     def test__get_kid_from_jwt(self, get_jwt):
         assert app._get_kid_from_jwt(get_jwt) == 'jwt_kid_salesforce_serviceX'
 
+        # test w/ invalid jwt protected header
+        split_protected_header = get_jwt.split('.')
+        split_protected_header[0] = split_protected_header[0] + 'badstring'
+        bad_jwt = '.'.join(split_protected_header)
+
+        kid = app._get_kid_from_jwt(bad_jwt)
+
+        if kid:
+            assert False, f'Should fail if protected header is malformed. Got kid "{kid}".'
+        else:
+            assert True, 'Failed as expected on malformed protected header.'
+
     def test__get_jwt_from_header(self, get_headers, get_jwt):
         assert app._get_jwt_from_header(get_headers) == get_jwt
 

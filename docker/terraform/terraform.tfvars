58bf34c694fae286fe4ee79670d20878a7f00f6f
pat <pat@p15r.net>
2020-12-08 20:45:10 +0100

    add monitoring tenant config

--- docker/terraform/terraform.tfvars
+++ docker/terraform/terraform.tfvars
@@ -1,7 +1,7 @@
-# salesforce tenant
 policies = ["salesforce", "monitoring"]
 
-auth_jwt_default_role                 = "distributey"
+# salesforce tenant
+auth_jwt_default_role                 = "salesforce"
 auth_jwt_default_role_bound_audiences = ["urn:distributey"]
 auth_jwt_validation_pubkeys = [LIST_OF_CERTS]
 
@@ -10,4 +10,8 @@ transit_exportable            = true
 
 # monitoring tenant
 auth_jwt_monitoring_default_role = "monitoring"
+auth_jwt_monitoring_default_role_bound_audiences = ["urn:distributey"]
 auth_jwt_monitoring_validation_pubkeys = [LIST_OF_MONITORING_CERTS]
+
+transit_monitoring_key_name = "monitoring"
+transit_monitoring_exportable = true
--- docker/terraform/variables.tf
+++ docker/terraform/variables.tf
@@ -1,6 +1,6 @@
 variable policies {
   type        = list
-  default     = ["salesforce"]
+  default     = []
   description = "List of policies for external service names. The name corresponds to the 'service' variable with suffix '-transit'"
 }
 
@@ -41,17 +41,14 @@ variable auth_jwt_monitoring_path {
   type    = string
   default = "jwt-monitoring"
 }
-
 variable auth_jwt_monitoring_validation_pubkeys {
   type    = list
   default = []
 }
-
 variable auth_jwt_monitoring_default_role {
   type    = string
   default = "role2"
 }
-
 variable auth_jwt_monitoring_default_role_bound_audiences {
   description = "List of aud claims to match against. Any match is sufficient."
   type        = list
@@ -65,5 +62,10 @@ variable transit_monitoring_path {
 
 variable transit_monitoring_key_name {
   type    = string
-  default = "monitoring"
+  default = "key2"
+}
+
+variable transit_monitoring_exportable {
+  type    = bool
+  default = false
 }

4385b6341edf68507e8bdd59b2ebdc936aa27e68
pat <pat@p15r.net>
2021-01-27 07:46:04 +0100

    WIP: 1st camouflage regex

--- distributey/dy_trace.py
+++ distributey/dy_trace.py
@@ -2,11 +2,46 @@
 
 import os
 import inspect
+from inspect import ArgInfo
 from typing import Optional, Any
 from types import FrameType
 import logging
+import re
 
 logger = logging.getLogger(__name__)
+
+
+# add all keys from log
+# add tracing
+# add unittest
+def __camouflage(func_args: ArgInfo):
+    # ArgInfo contains file descriptors and can therefore not be
+    # copy.deepcopy()-ed. (deepcopy pickles and unpickles object.
+    # during this process a file descriptor state will get lost,
+    # thus an exception is thrown that the object cannot be deepcopy()ed)
+    # as a hack, access a string representation,
+    # camouflage sensitive values and give back
+    # the string representation of the ArgInfo object
+
+    str_func_args = func_args.__repr__()
+
+    # still original objects that cannot be deepcopy()-ed
+    # d_func_args = func_args._asdict()
+    # d_func_args = func_args.__getnewargs__()
+
+    print(f'++++++++++++++++++++++ ORIG: {str_func_args}')
+
+    str_func_args = re.sub(r'\s+', ' ', str_func_args)   # remove all tabs, newlines and spaces
+
+    # replace: 'locals' -> 'header_args' -> 'jwt'
+    camouflaged_func_args = re.sub(
+        r'(.*)(locals)(.*)(header_args)(.*)(jwt\': \')([0-9a-zA-Z]*)([.\-_0-9a-zA-Z\s]*)(\'.*)',
+        r'\1\2\3\4\5\6******\9',
+        str_func_args)
+
+    print(f'############### REPLACED: {camouflaged_func_args}')
+
+    return camouflaged_func_args
 
 
 def __trace(current_frame: Optional[FrameType]) -> tuple:
@@ -23,22 +58,24 @@ def __trace(current_frame: Optional[FrameType]) -> tuple:
         logger.error('Failed to inspect frame: "%s".', current_frame)
         return ('error', 'error', 'error', 'error')
 
-    return func_name, func_args, file_name, line_no
+    str_func_args = __camouflage(func_args)
+
+    return func_name, str_func_args, file_name, line_no
 
 
 def trace_enter(current_frame: Optional[FrameType]) -> None:
     """Traces execution flow when entering a function/method."""
 
-    func_name, func_args, file_name, line_no = __trace(current_frame)
+    func_name, cp_func_args, file_name, line_no = __trace(current_frame)
 
     logger.info('(%s:%s) Entering "%s" args: %s',
-                file_name, line_no, func_name, func_args)
+                file_name, line_no, func_name, cp_func_args)
 
 
 def trace_exit(current_frame: Optional[FrameType], ret: Any) -> None:
     """Traces execution flow when exiting a function/method."""
 
-    func_name, func_args, file_name, line_no = __trace(current_frame)
+    func_name, cp_func_args, file_name, line_no = __trace(current_frame)
 
     logger.info('(%s:%s) Exiting "%s" ret: %s',
                 file_name, line_no, func_name, ret)

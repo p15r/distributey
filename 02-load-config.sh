963d6c43504077a7464a7f9d56dbe52cda832052
pat <pat@p15r.net>
2021-02-07 17:19:26 +0100

    remove "active" folder

--- 02-load-config.sh
+++ 02-load-config.sh
@@ -6,21 +6,16 @@ set -euf -o pipefail
 timestamp=$(date +%Y-%m-%d_%H:%M:%S)
 
 echo "🔧 Create \"config/\" directory if it does not exist.."
-docker exec -u root distributey sh -c "[ ! -d /opt/distributey/config/active ] && mkdir -p /opt/distributey/config/active/init || echo \"(Config directory already exists)\""
+docker exec -u root distributey sh -c "[ ! -d /opt/distributey/config/ ] && mkdir -p /opt/distributey/config/init || echo \"(Config directory already exists)\""
 
-echo "💾 Create backup of current config.."
-docker exec -u root distributey sh -c "mkdir /opt/distributey/config/config-$timestamp && cp -r /opt/distributey/config/active/* /opt/distributey/config/config-$timestamp"
+#echo "💾 Create backup of current config.."
+#docker exec -u root distributey sh -c "mkdir /opt/distributey/config/config-$timestamp && cp -r /opt/distributey/config/active/* /opt/distributey/config/config-$timestamp"
 
 echo "🗑️ Delete current config.."
-docker exec -u root distributey sh -c "rm -rf /opt/distributey/config/active/*"
+docker exec -u root distributey sh -c "rm -rf /opt/distributey/config/*"
 
 echo "💾 Copy new config.."
-docker cp config/. distributey:/opt/distributey/config/active/
+docker cp config/. distributey:/opt/distributey/config/
 
 echo "🔧 Set file permissions.."
-docker exec -u root distributey chown -R distributey:distributey /opt/distributey/config/active
-
-# gunicorn reloads only if a python file changes..
-#touch distributey/app.py
-#docker cp distributey/app.py distributey:/opt/distributey/app.py
-#docker exec -u root distributey chown -R distributey:distributey /opt/distributey/app.py
+docker exec -u root distributey chown -R distributey:distributey /opt/distributey/config/

e36858d545167901fde4a67e04c5737fae96a1f7
pat <pat@p15r.net>
2021-05-24 17:48:45 +0200

    improved formatting & docs

--- distributey/vault_backend.py
+++ distributey/vault_backend.py
@@ -11,6 +11,11 @@ immutable strings and can therefore not be safely erased from memory:
     hvac/adapters.py#L94
 - https://github.com/hvac/hvac/blob/b9343973307eaba1bbe28ebf9e1911520ffcbf0a/
     hvac/adapters.py#L287
+
+Vault Token Cache Note:
+Token expiration (TTL) is disregarded. In worst case scenario,
+every cache contains an expired token for a JWT KID. This would lead to
+"N worker processes + 1" HYOK request attempts to successfully authenticate.
 """
 
 import base64
@@ -22,9 +27,8 @@ from dy_trace import trace_enter, trace_exit, CAMOUFLAGE_SIGN
 import config
 import utils
 
-
-# In-memory cache per gunicorn worker process for Vault JWT tokens.
-# Because JWE requests are stateless (no cookies), access to any
+# In-memory cache, per gunicorn worker process, for Vault JWT tokens.
+# Because HYOK requests are stateless (no cookies), access to any
 # of the caches is random and so are cache hits/misses.
 __VAULT_TOKEN_CACHE: Dict[str, str] = {}
 
@@ -32,18 +36,20 @@ __VAULT_TOKEN_CACHE: Dict[str, str] = {}
 def __get_vault_client() -> hvac.Client:
     trace_enter(inspect.currentframe())
 
-    vault_mtls_client_cert = config.get_config_by_keypath(
-        'VAULT_MTLS_CLIENT_CERT')
-    vault_mtls_client_key = config.get_config_by_keypath(
-        'VAULT_MTLS_CLIENT_KEY')
+    vault_mtls_client_cert = \
+        config.get_config_by_keypath('VAULT_MTLS_CLIENT_CERT')
+    vault_mtls_client_key = \
+        config.get_config_by_keypath('VAULT_MTLS_CLIENT_KEY')
     vault_url = config.get_config_by_keypath('VAULT_URL')
-
     mtls_auth = (vault_mtls_client_cert, vault_mtls_client_key)
+    vault_ca_cert = config.get_config_by_keypath('VAULT_CACERT')
 
     try:
-        if vault_ca_cert := config.get_config_by_keypath('VAULT_CACERT'):
-            client = hvac.Client(cert=mtls_auth, url=vault_url,
-                                 verify=vault_ca_cert)
+        if vault_ca_cert:
+            client = hvac.Client(
+                cert=mtls_auth,
+                url=vault_url,
+                verify=vault_ca_cert)
         else:
             client = hvac.Client(cert=mtls_auth, url=vault_url, verify=True)
     except Exception as exc:
@@ -61,9 +67,10 @@ def __get_vault_token(
         tenant: str,
         priv_jwt_token: str,
         vault_auth_jwt_path: str,
-        cache_client_id: str
-) -> str:
-    # TODO: review + unittesting
+        cache_client_id: str) -> str:
+
+    trace_enter(inspect.currentframe())
+    # TODO: unittesting
     try:
         response = client.auth.jwt.jwt_login(
             role=config.get_vault_default_role_by_tenant(tenant),
@@ -75,7 +82,8 @@ def __get_vault_token(
         trace_exit(inspect.currentframe(), ret)
         return ret
 
-    logger.debug('Vault login response: %s', response)
+    if config.get_config_by_keypath('DEV_MODE'):
+        logger.debug('Vault login response: %s', response)
 
     try:
         vault_token = response['auth']['client_token']
@@ -83,10 +91,9 @@ def __get_vault_token(
         ret = ''
 
         logger.error(
-            'Failed to access Vault token from auth response: %s. '
+            'Failed to access the Vault token from auth response: %s. '
             'This is most likely a permission issue.',
-            exc
-        )
+            exc)
 
         trace_exit(inspect.currentframe(), ret)
         return ret
@@ -94,7 +101,7 @@ def __get_vault_token(
     if config.get_config_by_keypath('DEV_MODE'):
         logger.debug('Vault client token returned: %s', vault_token)
 
-    logger.debug('Fetched new Vault token.')
+    logger.debug('Retrieved new Vault token.')
 
     __VAULT_TOKEN_CACHE[cache_client_id] = vault_token
 
@@ -102,37 +109,39 @@ def __get_vault_token(
     return vault_token
 
 
-def __authenticate_vault_client(client: hvac.Client, tenant: str,
-                                priv_jwt_token: str) -> hvac.Client:
+def __authenticate_vault_client(
+        client: hvac.Client,
+        tenant: str,
+        priv_jwt_token: str) -> hvac.Client:
+
     trace_enter(inspect.currentframe())
 
     vault_auth_jwt_path = config.get_vault_auth_jwt_path_by_tenant(tenant)
 
-    logger.debug('Attempting to authenticate against Vault using JWT: %s',
-                 priv_jwt_token)
+    logger.debug(
+        'Attempting to authenticate against Vault using JWT: %s',
+        priv_jwt_token)
 
     cache_client_id = utils.get_kid_from_jwt(priv_jwt_token)
 
     if cache_client_id in __VAULT_TOKEN_CACHE:
         logger.debug(
-            'Cache hit: Found token for "%s".', cache_client_id
-        )
+            'Cache hit: Found token for "%s".', cache_client_id)
         client.token = __VAULT_TOKEN_CACHE[cache_client_id]
     else:
         logger.debug(
-            'Cache miss: No token for "%s" exists.', cache_client_id
-        )
+            'Cache miss: Token for "%s" not found.', cache_client_id)
+
         token = __get_vault_token(
             client,
             tenant,
             priv_jwt_token,
             vault_auth_jwt_path,
-            cache_client_id
-        )
+            cache_client_id)
 
         if not token:
             ret = None
-            logger.error('Invalid token.')
+            logger.error('Failed to get Vault token.')
             trace_exit(inspect.currentframe(), ret)
             return ret
 
@@ -145,10 +154,9 @@ def __authenticate_vault_client(client: hvac.Client, tenant: str,
         ret = None
 
         logger.error(
-            'Attempt to authenticate against Vault failed. '
-            'Review configuration (config/config.json).'
-            'Deleted token from cache.'
-        )
+            'Failed to validate Vault client. '
+            'Review configuration (config/config.json). '
+            'Retry as token might have expired.')
 
         trace_exit(inspect.currentframe(), ret)
         return ret
@@ -157,9 +165,11 @@ def __authenticate_vault_client(client: hvac.Client, tenant: str,
     return client
 
 
-def get_dynamic_secret(tenant: str, key: str, key_version: str,
-                       priv_jwt_token: str) -> bytearray:
+def get_dynamic_secret(
+        tenant: str, key: str, key_version: str,
+        priv_jwt_token: str) -> bytearray:
     """Fetches dynamic secret from Vault."""
+
     trace_enter(inspect.currentframe())
 
     vault_transit_path = config.get_vault_transit_path_by_tenant(tenant)
@@ -175,7 +185,7 @@ def get_dynamic_secret(tenant: str, key: str, key_version: str,
     client = __authenticate_vault_client(client, tenant, priv_jwt_token)
     if not client:
         ret = bytearray()
-        logger.error('Failed to create Vault client.')
+        logger.error('Failed to authenticate Vault client.')
         trace_exit(inspect.currentframe(), ret)
         return ret
 
@@ -201,8 +211,9 @@ def get_dynamic_secret(tenant: str, key: str, key_version: str,
         except KeyError as exc:
             ret = bytearray()
 
-            logger.error('Failed to access key version in Vault key read '
-                         'response: %s', exc)
+            logger.error(
+                'Failed to access key version in Vault key read '
+                'response: %s', exc)
 
             trace_exit(inspect.currentframe(), ret)
             return ret
@@ -210,7 +221,9 @@ def get_dynamic_secret(tenant: str, key: str, key_version: str,
     # fetch key
     try:
         response = client.secrets.transit.export_key(
-            name=key, key_type='encryption-key', version=key_version,
+            name=key,
+            key_type='encryption-key',
+            version=key_version,
             mount_point=vault_transit_path)
     except Exception as exc:
         ret = bytearray()
@@ -226,8 +239,8 @@ def get_dynamic_secret(tenant: str, key: str, key_version: str,
     except Exception as exc:
         ret = bytearray()
 
-        logger.error('Failed to get key material from response: %s',
-                     exc)
+        logger.error(
+            'Failed to get key material from response: %s', exc)
 
         trace_exit(inspect.currentframe(), ret)
         return ret

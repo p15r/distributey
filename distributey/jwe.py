24f97f2fd6e860081b65a1d4310d731653cc62f5
pat <pat@p15r.net>
2021-11-22 07:39:23 +0000

    refactor: make get_key_consumer_cert more readable

--- distributey/jwe.py
+++ distributey/jwe.py
@@ -34,19 +34,22 @@ def _get_key_consumer_cert(tenant: str, jwe_kid: str) -> str:
     trace_enter(inspect.currentframe())
 
     # try to fetch dedicated key consumer cert
-    if not (key_consumer_cert_path :=
-            config.get_key_consumer_cert_by_tenant_and_kid(tenant, jwe_kid)):
+    key_consumer_cert_path = config.get_key_consumer_cert_by_tenant_and_kid(
+        tenant,
+        jwe_kid
+    )
+
+    if not key_consumer_cert_path:
         logger.warning('Cannot find dedicated key consumer certificate '
-                       'for "%s/%s". Configure it in '
-                       'config/config.json.',
+                       'for "%s/%s". Searching for backend-wide key.',
                        tenant, jwe_kid)
 
-        # fall back to backend-wide configured key consumer cert
-        ret = ''
+        # fall back to backend-wide key consumer cert
+        key_consumer_cert_path = \
+            config.get_backend_wide_key_consumer_cert_by_tenant(tenant)
 
-        if not (key_consumer_cert_path :=
-                config.get_backend_wide_key_consumer_cert_by_tenant(tenant)):
-
+        if not key_consumer_cert_path:
+            ret = ''
             logger.error('Cannot find dedicated key consumer certificate'
                          'nor backend-wide key consumer certificate for'
                          '"%s/%s"', tenant, jwe_kid)

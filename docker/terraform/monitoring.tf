61b56320aae2a0df1e06828c627406269284ef5a
Andreas Gruhler <andreas.gruhler@adfinis.com>
2021-11-25 15:30:57 +0100

    add root namespace monitoring
    This adds resources in the Vault root namespace for monitoring or
    testing access in the root namespace (e.g., for Vault OSS).

--- docker/terraform/monitoring.tf
+++ docker/terraform/monitoring.tf
@@ -29,3 +29,37 @@ resource "vault_transit_secret_backend_key" "monitoring" {
   exportable = var.transit_exportable
   provider   = vault.tenant
 }
+
+# JWT auth backend for monitoring in the root namespace
+resource "vault_jwt_auth_backend" "jwt_monitoring_root" {
+  path                   = var.auth_jwt_monitoring_path
+  jwt_validation_pubkeys = var.auth_jwt_monitoring_validation_pubkeys
+  default_role           = var.auth_jwt_monitoring_default_role
+}
+
+resource "vault_jwt_auth_backend_role" "role_monitoring_root" {
+  backend         = vault_jwt_auth_backend.jwt_monitoring_root.path
+  role_name       = var.auth_jwt_monitoring_default_role
+  bound_audiences = var.auth_jwt_monitoring_default_role_bound_audiences
+  user_claim      = "iss"
+  role_type       = "jwt"
+  token_policies  = ["default", "monitoring"]
+}
+
+# Transit engine for monitoring key
+resource "vault_mount" "transit_monitoring_root" {
+  path     = var.transit_monitoring_path
+  type     = "transit"
+}
+
+resource "vault_transit_secret_backend_key" "monitoring_root" {
+  backend    = vault_mount.transit_monitoring_root.path
+  name       = var.transit_monitoring_key_name
+  exportable = var.transit_exportable
+}
+
+# Vault policy for monitoring transit engine in the root namespace
+resource "vault_policy" "policy_root" {
+  name     = "monitoring"
+  policy   = file("policies/monitoring.hcl")
+}

bec03e7509f9dba60821f82c1ec7dc0e6d022ad1
pat <pat@p15r.net>
2022-05-26 10:36:20 +0000

    fix: improved error reporting in scripts

--- 00-build.sh
+++ 00-build.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
-set -euf -o pipefail
+set -o pipefail
+set -o errexit
+
+err_report() {
+    echo "Error on line $(caller)" >&2
+    awk 'NR>L-4 && NR<L+4 { printf "%-5d%3s%s\n",NR,(NR==L?">>>":""),$0 }' L=$1 $0
+}
+
+trap 'err_report $LINENO' ERR
 
 function help {
     echo -e "-h\tShow help."
     echo -e "-d\tEnable developer mode."
     exit 0
 }
+
+# check if docker is running
+docker ps > /dev/null
 
 dev_mode=false
 

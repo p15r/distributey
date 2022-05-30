c7dd5d201223141207062cb90486719d8c3a42dc
Pat <pat@p15r.net>
2022-05-30 07:13:01 +0200

    fix: improved error reporting in scripts (#120)

--- 01-fix-cfg-perms.sh
+++ 01-fix-cfg-perms.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
-set -euf -o pipefail
+set -o pipefail
+set -o errexit
+
+err_report() {
+    echo "Error on line $(caller)" >&2
+    awk 'NR>L-4 && NR<L+4 { printf "%-5d%3s%s\n",NR,(NR==L?">>>":""),$0 }' L=$1 $0
+}
+
+trap 'err_report $LINENO' ERR
+
+
 
 echo "🔧 Set file permissions.."
 find config/ -type d -exec chmod o=rx {} \;
--- 03-stop.sh
+++ 03-stop.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-set -euf -o pipefail
+set -o pipefail
+set -o errexit
+
+err_report() {
+    echo "Error on line $(caller)" >&2
+    awk 'NR>L-4 && NR<L+4 { printf "%-5d%3s%s\n",NR,(NR==L?">>>":""),$0 }' L=$1 $0
+}
+
+trap 'err_report $LINENO' ERR
 
 echo '🛬 Stopping distributey...'
 docker-compose --compatibility stop
